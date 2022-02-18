7c5995c41caaa0f9905799fd04be690a8091f850
Minh Tri Truong <ngoc-minh-tri.truong@imt-atlantique.net>
2022-02-18 14:45:46 +0100

    Implemented Fan class

--- fan.h
+++ fan.h
@@ -7,4 +7,12 @@ public:
 	~Fan();
 	void update() override final;
 	void info() override final;
+	bool isOn() const;
+private:
+	bool _isOn;
 };
+
+
+inline std::ostream& operator<< (std::ostream& flot, const Fan& fan) {
+	flot << "Fan is on:" << fan.isOn();
+}
